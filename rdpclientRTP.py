a6675090ce6c26c7d0dd063b3d0eeee863454e58
Gabriel Galarza <gabrgalarza@gatech.edu>
2016-03-26 16:29:37 -0400

    Added RTP Exception class and modified db RTP files

--- rdpclientRTP.py
+++ rdpclientRTP.py
@@ -1,4 +1,5 @@
 import socket
+from collections import namedtuple
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
@@ -8,6 +9,9 @@ print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
 print "message:", MESSAGE
 
+MyStruct = namedtuple("MyStruct", "data ACK SEQ windowSize destination")
+mess = MyStruct(data = MESSAGE, ACK = 0, SEQ = 0, windowSize = 1, destination = UDP_IP)
+
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(mess, (UDP_IP, UDP_PORT))
